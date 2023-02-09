60b19dec2653a65a6325dbf485ecf55b79745b66
Pascal Paulis <ppaulis@gmail.com>
2023-02-09 17:09:51 +0100

    moved identity classes/interfaces to this repository
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

---
+++ src/JwtAuthentication/MachineUserIdentity.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BayWaReLusy\JwtAuthentication;
+
+class MachineUserIdentity implements IdentityInterface
+{
+    use IdentityTrait;
+
+    public const CONSOLE_APPLICATION = 'console';
+
+    protected string $applicationId;
+
+    /**
+     * @return string
+     */
+    public function getRoleId()
+    {
+        return 'm2m_' . $this->getApplicationId();
+    }
+
+    /**
+     * @return string
+     */
+    public function getApplicationId(): string
+    {
+        return $this->applicationId;
+    }
+
+    /**
+     * @param string $applicationId
+     * @return MachineUserIdentity
+     */
+    public function setApplicationId(string $applicationId): MachineUserIdentity
+    {
+        $this->applicationId = $applicationId;
+        return $this;
+    }
+}
---
+++ src/JwtAuthentication/UserIdentity.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace BayWaReLusy\JwtAuthentication;
+
+/**
+ * Class UserIdentity
+ */
+class UserIdentity implements IdentityInterface
+{
+    use IdentityTrait;
+
+    protected string $id;
+    protected string $username;
+    protected string $email;
+    protected bool $emailVerified;
+    protected ?\DateTime $created;
+    /** @var string[] */
+    protected array $roles = [];
+
+    /**
+     * @return string
+     */
+    public function getRoleId(): string
+    {
+        return 'user_' . $this->getId();
+    }
+
+    /**
+     * @param Token $jwtToken
+     * @return UserIdentity
+     */
+    public static function createFromJWT(Token $jwtToken): UserIdentity
+    {
+        $identity = new UserIdentity();
+        $identity
+            ->setId($jwtToken->getSub())
+            ->setUsername($jwtToken->getUsername())
+            ->setEmailVerified($jwtToken->getEmailVerified())
+            ->setEmail($jwtToken->getEmail())
+            ->setRoles($jwtToken->getRoles());
+
+        $identity->setScopes($jwtToken->getScopes());
+
+        return $identity;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): UserIdentity
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): UserIdentity
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): UserIdentity
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getEmailVerified(): bool
+    {
+        return $this->emailVerified;
+    }
+
+    public function setEmailVerified(bool $emailVerified): UserIdentity
+    {
+        $this->emailVerified = $emailVerified;
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTime
+    {
+        return $this->created;
+    }
+
+    public function setCreated(?\DateTime $created): UserIdentity
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): UserIdentity
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function addRole(string $role): UserIdentity
+    {
+        if (!in_array($role, $this->roles)) {
+            $this->roles[] = $role;
+        }
+
+        return $this;
+    }
+}
